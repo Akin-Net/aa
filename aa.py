1d482eca3954eeeed55e2dccce01540c7cc551a6
Danilo Shiga <daniloshiga@gmail.com>
2011-07-19 13:15:00 -0300

    changed the instructions and some comments to follow the wiki definitions

--- aa.py
+++ aa.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Cliente para enviar mensagens ao servidor
+# http://wiki.nosdigitais.teia.org.br/AA_0.0.1
 
 import sys
 import os
@@ -12,21 +13,21 @@ Administrador da Ágora
 
 Uso:
 
-   aa inicio            = avisa que começou as atividades do dia
-   aa anota <resumo>    = anota o que está fazendo em uma mensagem curta
-   aa fim               = avisa que terminou as atividades do dia
+   aa start            = avisa que começou as atividades do dia
+   aa alert <resumo>    = anota o que está fazendo em uma mensagem curta
+   aa stop               = avisa que terminou as atividades do dia
   
 """
 
 def direciona(args):
     if args[0] in ['inicio', 'inicia', 'início', 'start', 'begin']:
-        # conecta no servidor e registra hora de início
+        # registra hora de início
         pass
     elif args[0] in ['fim', 'finaliza', 'termina', 'end']:
-        # conecta no servidor e registra hora de fim
+        # registra hora de fim
         pass
     elif args[0] in ['informa', 'marca', 'anota', 'msg']:
-        # conecta no servidor e registra marca no registro iniciado (corrente)
+        # registra marca no registro iniciado (corrente)
         pass
     else:
         print 'Opção "%s" inválida!' % args[0]
