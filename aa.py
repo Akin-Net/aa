8511bfd17e3b53761c8412a23a9f3199d677e9c2
Danilo Shiga <daniloshiga@gmail.com>
2011-07-19 13:29:30 -0300

    added log function to write the temp file with data before sending to server

--- aa.py
+++ aa.py
@@ -21,16 +21,28 @@ Uso:
 
 def direciona(args):
     if args[0] in ['inicio', 'inicia', 'início', 'start', 'begin']:
+        log('start')
         # registra hora de início
         pass
     elif args[0] in ['stop','fim', 'finaliza', 'termina', 'end']:
+        log('stop')
         # registra hora de fim
         pass
     elif args[0] in ['alert','informa', 'marca', 'anota', 'msg']:
+        log('alert')
         # registra marca no registro iniciado (corrente)
         pass
     else:
         print 'Opção "%s" inválida!' % args[0]
+
+def log(msg):
+    f = open("/tmp/.aa.txt","a")
+    try:
+        f.writelines(msg+"\n")
+    finally:
+        f.close()
+    return
+
 
 if __name__=="__main__":
     if len(sys.argv) > 1:
