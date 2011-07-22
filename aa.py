0b13f868902fcf28eb3417a135d40c8e3a4ef3e1
AndresMRM <andresmrm@gmail.com>
2011-07-22 15:29:50 -0300

    passado codigo para Python3 e adicionada uma classe para cuidar do Timer

--- aa.py
+++ aa.py
@@ -1,10 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------
+# Copyright 2011 Lab Macambira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#-----------------------------------------------------------------------------
 
 # Cliente para enviar mensagens ao servidor
 # http://wiki.nosdigitais.teia.org.br/AA_0.0.1
 
-import urllib, urllib2
+import urllib
 import sys
 import os
 from time import time, strftime
@@ -39,8 +56,8 @@ def termina():
     msg = {'user': os.getenv('NICKNAME'), 'log': str[0]+'::'+str[1]}
     dados = urllib.urlencode(msg)
     # sends the string
-    req = urllib2.Request('http://nightsc.com.br/aa/novo_log.php', dados)
-    res = urllib2.urlopen(req)
+    req = urllib.request('http://nightsc.com.br/aa/novo_log.php', dados)
+    res = urllib.urlopen(req)
     pagina = res.read()
     str = f.readline()
 
@@ -52,19 +69,20 @@ def direciona(args):
     if args[0] in ['start','inicio', 'inicia', 'início', 'begin']:
         comeca()
         log('start')
-        print '[AA] Your session has started. Happy hacking!'
+        print('[AA] Your session has started. Happy hacking!')
     elif args[0] in ['stop','fim', 'finaliza', 'termina', 'end']:
         # registra hora de fim
         log('stop')
         termina()
-        print '[AA] You ended the sesssion and published at http://nightsc.com.br/aa. CYA!'
+        print('[AA] You ended the sesssion and published at' \
+        'http://nightsc.com.br/aa. CYA!')
     elif args[0] in ['alert','informa', 'marca', 'anota', 'msg'] and args[1]:
         # registra marca no registro iniciado (corrente)
         #FIXME só funciona se a mensagem estiver entre parenteses:
         log("alert "+sys.argv[2:][0])
-        print '[AA] New alert: "%s" logged.' % sys.argv[2:][0]
+        print('[AA] New alert: "%s" logged.' % sys.argv[2:][0])
     else:
-        print 'Opção "%s" inválida!' % args[0]
+        print('Opção "%s" inválida!' % args[0])
 
 def log(msg):
     """Saves messages on the ~/.aa.txt temp file"""
@@ -80,8 +98,36 @@ def log(msg):
         f.close()
     return
 
+
+class Sentinela(object):
+    """Warnings from time to time"""
+
+    def __init__(self, minutos=15):
+        """Give a warning after each minutos"""
+        self.minutos = minutos
+        self.timer = Timer(self.minutos*60, self.avisar)
+
+    def iniciar(self):
+        """Starts to count"""
+        self.timer.start()
+
+    def parar(self):
+        """Stops to count"""
+        self.timer.cancel()
+
+    def avisar(self):
+        """Gives the warning and restarts the Timer"""
+        print("hello, world")
+        self.timer = Timer(self.minutos*60, self.avisar)
+        self.timer.start()
+
+
+s = Sentinela(0.1)
+s.iniciar()
+
+
 if __name__=="__main__":
     if len(sys.argv) > 1:
         direciona(sys.argv[1:])
     else:
-        print instrucoes
+        print(instrucoes)
