0dfedce659b0562f107571a5c33ce358164fdebb
Alexandre Negrão <akin@grad.icmc.usp.br>
2011-07-25 10:23:43 -0300

    Applying Andres patch

--- aa.py
+++ aa.py
@@ -1,10 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------
+# Copyright 2011 Lab Macambira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#-----------------------------------------------------------------------------
 
 # Cliente para enviar mensagens ao servidor
 # http://wiki.nosdigitais.teia.org.br/AA_0.0.1
 
-import urllib, urllib2
+# deps:
+# libnotify-bin
+
+import urllib.request
+import urllib.parse
 import sys
 import os
 from time import time, strftime
@@ -21,30 +42,99 @@ Using:
   
 """
 
+Sent = None
+Envi = None
+
+
+class Sentinela(object):
+    """Warnings from time to time"""
+
+    def __init__(self, minutos=15):
+        """Give a warning after each minutos"""
+        self.minutos = minutos
+        self.vezes = -1
+
+    def iniciar(self):
+        """Starts to count"""
+        #self.timer.start()
+        self.avisar()
+
+    def parar(self):
+        """Stops to count"""
+        self.timer.cancel()
+        print("PAROU!!!!!!!!!")
+
+    def avisar(self):
+        """Gives the warning and restarts the Timer"""
+        self.vezes = self.vezes + 1
+        os.system('notify-send "AA:" "WTF are you doing?!\n(Trabalhando há '+\
+                  str(self.minutos*self.vezes) + ' minutos)"')
+        self.timer = Timer(self.minutos*60, self.avisar)
+        self.timer.start()
+
+
+class Enviador(Sentinela):
+    """Sends the data to the server periodically"""
+
+    def avisar(self):
+        """Sends data"""
+        global Envi
+        fim = enviar()
+        if not fim:
+            self.timer = Timer(self.minutos*60, self.avisar)
+            self.timer.start()
+        else:
+            Sent.parar()
+    
+
 def comeca():
-  """Start the session"""
-  home = os.getenv("HOME")
-  f = open(home+"/.aa.txt", "w")
-  f.close()
+    """Start the session"""
+    home = os.getenv("HOME")
+    f = open(home+"/.aa.txt", "w")
+    f.close()
+    os.system("aa daemon &")
+
+def enviar():
+    """Sends data to the server"""
+    fim = False
+    home = os.getenv("HOME")
+    f = open(home+"/.aa.txt", "r")
+    alertas = f.read().splitlines()
+    f.close()
+    # FIXME cuidar de possíveis problemas de concorrência
+    f = open(home+"/.aa.txt", "w")
+    f.close()
+    for alerta in alertas:
+        # prepare the string
+        alerta = alerta.split(',')
+        msg = {'user': os.getenv('NICKNAME'), 'log': alerta[0]+'::'+alerta[1]}
+        dados = urllib.parse.urlencode(msg)
+        # sends the string
+        print("Sending:",alerta[1])
+        req = urllib.request.Request('http://nightsc.com.br/aa/novo_log.php',
+                                     dados.encode('ascii'))
+        res = urllib.request.urlopen(req)
+        #pagina = res.read()
+        res.close()
+        # prepare the flag that will stop the daemon
+        if alerta[1] == 'stop':
+            fim = True
+    return fim
 
 def termina():
-  """Stop the session"""
-  home = os.getenv("HOME")
-  f = open(home+"/.aa.txt", "r")
-  str = f.readline()
-  while (len(str) > 0):
-    # prepare the string
-    str = str.split('\n')
-    str = str[0].split(',')
-    msg = {'user': os.getenv('NICKNAME'), 'log': str[0]+'::'+str[1]}
-    dados = urllib.urlencode(msg)
-    # sends the string
-    req = urllib2.Request('http://nightsc.com.br/aa/novo_log.php', dados)
-    res = urllib2.urlopen(req)
-    pagina = res.read()
-    str = f.readline()
+    """Stop the session"""
+    #enviar()
+    pass
 
-  res.close()
+# FIXME criar daemon de forma que ele não morra junto com o terminal
+def daemonificar():
+    """Runs the daemon"""
+    global Sent, Envi
+    # FIXME permitir configurar esse tempo
+    Sent = Sentinela(15)
+    Sent.iniciar()
+    Envi = Enviador(0.25)
+    Envi.iniciar()
 
 def direciona(args):
     """Parse AA arguments"""
@@ -52,19 +142,25 @@ def direciona(args):
     if args[0] in ['start','inicio', 'inicia', 'início', 'begin']:
         comeca()
         log('start')
-        print '[AA] Your session has started. Happy hacking!'
+        print('[AA] Your session has started. Happy hacking!')
     elif args[0] in ['stop','fim', 'finaliza', 'termina', 'end']:
         # registra hora de fim
         log('stop')
         termina()
-        print '[AA] You ended the sesssion and published at http://nightsc.com.br/aa. CYA!'
+        print('[AA] You ended the sesssion and published at ' \
+        'http://nightsc.com.br/aa. CYA!')
+    elif args[0] in ['daemon']:
+        # starts the daemon
+        daemonificar()
+        print('[AA] Daemon started')
     elif args[0] in ['alert','informa', 'marca', 'anota', 'msg'] and args[1]:
         # registra marca no registro iniciado (corrente)
-        #FIXME só funciona se a mensagem estiver entre parenteses:
-        log("alert "+sys.argv[2:][0])
-        print '[AA] New alert: "%s" logged.' % sys.argv[2:][0]
+        msg = ''.join([pal+" " for pal in sys.argv[2:]])
+        msg = msg.strip()
+        log("alert "+msg)
+        print('[AA] New alert: "%s" logged.' % msg)
     else:
-        print 'Opção "%s" inválida!' % args[0]
+        print('Opção "%s" inválida!' % args[0])
 
 def log(msg):
     """Saves messages on the ~/.aa.txt temp file"""
@@ -80,8 +176,13 @@ def log(msg):
         f.close()
     return
 
+
+
+
+
+
 if __name__=="__main__":
     if len(sys.argv) > 1:
         direciona(sys.argv[1:])
     else:
-        print instrucoes
+        print(instrucoes)
